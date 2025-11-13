8f5b2ad92fe990a6c091e341a9b428e58772e32e
AlfaDriveCentre <ilkay@alfadrive.com.tr>
2025-11-13 11:24:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 // basit burger menü için küçük bir kanca burada bulunuyor
+function toggleNav() {
+  var n = document.getElementById("main-nav");
+  if (!n) return;
+  n.classList.toggle("open");
+}
